cdaa38fad0acc140f40e25bb7a9547dd5bdc6dd1
alfonsserra <alfonsoserra@gmail.com>
2019-01-07 17:30:28 +0100

    Restore sample files

---
+++ src/test/java/com/systelab/sti/architecture/ControllerTest.java
@@ -0,0 +1,34 @@
+package com.systelab.sti.architecture;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.systelab.architecture.rules.ClassesTransformers.methods;
+import static com.systelab.architecture.rules.methods.MethodConditionUtilities.*;
+import static com.systelab.architecture.rules.methods.MethodPredicateUtilities.arePublic;
+import static com.systelab.architecture.rules.methods.MethodPredicateUtilities.inClassesAnnotatedWith;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
+
+@AnalyzeClasses(packages = "com.systelab.sti")
+public class ControllerTest {
+
+    @ArchTest
+    static ArchRule allFilesInPackageControllerShouldBeControllersOrDtos = classes()
+            .that().resideInAPackage("..controller..")
+            .should().haveSimpleNameEndingWith("Controller").orShould().haveSimpleNameEndingWith("Dto")
+            .because("we want to have only controllers and dtos in the controllers folder");
+
+    @ArchTest
+    static ArchRule allPublicMethodsInRestControllersShouldReturnResponseEntity =
+            all(methods())
+                    .that(inClassesAnnotatedWith(Deprecated.class)).and(arePublic())
+                    .should(returnType(String.class))
+                    .because("we don't want to couple the client code directly to the return types of the encapsulated module");
+
+    @ArchTest
+    static ArchRule controllersShouldNotAccessRepositories = noClasses()
+            .that().resideInAPackage("..controller..")
+            .should().accessClassesThat().resideInAPackage("..repository..")
+            .because("we want controller classes to do not access repository classes directly");
+}
---
+++ src/test/java/com/systelab/sti/architecture/DtoTest.java
@@ -0,0 +1,18 @@
+package com.systelab.sti.architecture;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.systelab.architecture.rules.classes.ClassConditionUtilities.haveAllFieldsPublic;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+@AnalyzeClasses(packages = "com.systelab")
+public class DtoTest {
+
+    @ArchTest
+    static ArchRule dtoClassesShouldHaveAllFieldsPublic = classes()
+            .that().areAnnotatedWith(Deprecated.class)
+            .should(haveAllFieldsPublic())
+            .because("Dto classes should have all fields as public as we do not want any kind of logic inside");
+}
